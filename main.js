2e24734b6bebbfb621447ae83a8ab088727483a2
SofiaFesenko <108746952+SofiaFesenko@users.noreply.github.com>
2023-02-20 23:01:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+for(let i = 4; i <= 15; i++) {
+    console.log(i)
+}
+
+for(let i = 1; i <= 8; i++) {
+    console.log("8")
+}
+
+numb = Number(prompt("write a number"))
+for(let i = 1; i <= numb; i++) {
+    console.log("hi", i)
+}
+
+
+
+// for(let i = 1; i <= 15; i++) {
+//     console.log("hi", i)
+// }
+
+// for(let i = 2; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// for(let i = 15; i >= 0; i--) {
+//     console.log(i)
+// }
+
+// for(let i = 1; i <= 10; i++) {
+//     let numb = Number(prompt("enter a number"))
+//     if (numb > 10) {
+//         alert("ok")
+//     }
+//     else {
+//         alert("not ok")
+//     }
+// }
+
+// for(let i = 1; i <= 10; i++) {
+//     if (i != 7) {
+//         console.log(i)
+//     }
+// }
